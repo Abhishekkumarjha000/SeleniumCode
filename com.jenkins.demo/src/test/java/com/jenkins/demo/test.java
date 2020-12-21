d3607610c3115a458c0beed1ecb86633a3fe55a6
Abhishek.kumar <Abhishek.kumar@SAGITEC-0598.corp.sagitec.com>
2020-12-21 18:42:56 +0530

    restapi deseriliazation

---
+++ com.jenkins.demo/src/test/java/com/jenkins/demo/test.java
@@ -0,0 +1,27 @@
+package com.jenkins.demo;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+
+public class test { 	
+	@Test
+
+	public void testng() {
+	
+		
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		//System.setProperty("webdriver.firefox.driver", "./drivers/geckodriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+	
+		 	 	
+		//add comment
+		
+
+	}
+
+}
